50fd1eba80725fc2639b41b7b2f111e9e5b93fe7
yanuvelir <yanuvelir@gmail.com>
2024-07-02 17:41:43 +0300

    initial commit2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     private JFrame frame;
 
