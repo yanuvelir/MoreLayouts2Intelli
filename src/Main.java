1747de2ef408cd91dd8d5b61188776e38591e2f4
yanuvelir <yanuvelir@gmail.com>
2024-07-02 17:36:06 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private JFrame frame;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Main window = new Main();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public Main() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 450, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS));
+
+
+        JButton btn1 = new JButton("btn1");
+
+
+
+        JButton btn2 = new JButton("btn2");
+
+
+        frame.add(btn1);
+        frame.add(Box.createGlue());
+        frame.add(btn2);
+        //box.add(Box.createHorizontalStrut(15));
+    }
+}
